37be6b0fa2f00c03285a9c3853477cde30bf178b
Frank Hassel <462160@student.fontys.nl>
2020-11-24 11:16:14 +0100

    SmartFoodChoiceApp toegevoegd

---
+++ SmartFoodChoice/SmartFoodChoiceApp/SmartFoodChoiceApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SmartFoodChoiceApp.Services;
+using SmartFoodChoiceApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartFoodChoiceApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SmartFoodChoice/SmartFoodChoiceApp/SmartFoodChoiceApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SmartFoodChoiceApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SmartFoodChoiceApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
