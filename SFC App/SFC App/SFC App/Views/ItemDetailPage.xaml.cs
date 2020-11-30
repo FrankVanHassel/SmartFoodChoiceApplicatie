6cfdb89e9ba0b12f72920e34e3883714918dc0b2
rikdgd <rik.dgd@gmail.com>
2020-11-30 09:49:09 +0100

    App template added

---
+++ SFC App/SFC App/SFC App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SFC_App.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SFC_App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ SFC App/SFC App/SFC App/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SFC_App.Models;
+using SFC_App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SFC_App.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
