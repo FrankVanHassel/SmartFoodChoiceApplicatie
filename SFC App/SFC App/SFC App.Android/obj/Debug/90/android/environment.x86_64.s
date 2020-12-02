d1e06c6a43ac7ccb733bd061c18c8120911cd4d8
rikdgd <rik.dgd@gmail.com>
2020-12-02 14:04:47 +0100

    Add favorites page
    Added the last page to the app, this page does not have any content.

--- SFC App/SFC App/SFC App.Android/obj/Debug/90/android/environment.x86_64.s
+++ SFC App/SFC App/SFC App.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"e0a295d6-f614-4631-90b1-f0a358a9093f"
+	.asciz	"37e21b11-8d80-4624-9d57-1e2f9545667f"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
