4bbd3fd5215df182e134dd53ae1bf87e163250dc
rikdgd <rik.dgd@gmail.com>
2020-12-16 10:07:27 +0100

    Added DataHandler class
    Also made some other changes in the code but none of this is finished

---
+++ SFC App/SFC App/SFC App/DataHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFC_App
+{
+    public class DataHandler
+    {
+        char splitter = '/';
+
+        /// <summary>
+        /// Converts a string with data sepperated by a '/' to a list of strings.
+        /// </summary>
+        /// <param name="dataString">The string with data</param>
+        /// <returns>List with all seperated data</returns>
+        public List<string> DataToList(string dataString)
+        {
+            List<string> dataList = new List<string>();
+            string memoryString = "";
+            
+
+            foreach(char a in dataString)
+            {
+                if (a == splitter)
+                {
+                    dataList.Add(memoryString);
+                    memoryString = "";
+                }
+                else
+                {
+                    memoryString += a;
+                }
+            }
+
+            dataList.Add(memoryString);
+
+            return dataList;
+        }
+
+        /// <summary>
+        /// Converts a list of strings to a string with all elements seperated by a '/'
+        /// </summary>
+        /// <param name="dataList">The list containing data</param>
+        /// <returns>A string with data, seperated by a '/'</returns>
+        public string ListToData(List<string> dataList)
+        {
+            string totalString = "";
+
+            foreach(string element in dataList)
+            {
+                totalString += element;
+                totalString += splitter;
+            }
+
+            return totalString;
+        }
+    }
+}
--- SFC App/SFC App/SFC App/ServerConnection.cs
+++ SFC App/SFC App/SFC App/ServerConnection.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace SFC_App
 {
     public class ServerConnection
     {
-        string serverIP = "145.93.144.80";
+        string serverIP = "192.168.174.189";    // The IP address from the ubuntu server
         int port = 8080;
 
         public void SendData(List<string> data)
         {
+            // Convert the data from a list to a string
+            DataHandler dataConvert = new DataHandler();
+            string dataString = dataConvert.ListToData(data);
 
+            // Create new tcp client, and get the amount of bytes in the message
+            TcpClient client = new TcpClient(serverIP, port);
+            int byteCount = Encoding.ASCII.GetByteCount(dataString);
+
+            // Create the buffer to send via tcp
+            byte[] buffer = new byte[byteCount];
+            buffer = Encoding.ASCII.GetBytes(dataString);
+
+            // Get the stream and use it to send the buffer to the server
+            NetworkStream stream = client.GetStream();
+            stream.Write(buffer, 0, buffer.Length);
         }
     }
 }
