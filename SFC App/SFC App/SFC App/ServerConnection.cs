9a459dbe571ddcb34995eda1968192c58b76e283
rikdgd <rik.dgd@gmail.com>
2020-12-16 09:27:58 +0100

    final 2
    forgot something from stashed

--- SFC App/SFC App/SFC App/ServerConnection.cs
+++ SFC App/SFC App/SFC App/ServerConnection.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace SFC_App
 {
@@ -9,9 +11,42 @@ namespace SFC_App
         string serverIP = "145.93.144.80";
         int port = 8080;
 
+
         public void SendData(List<string> data)
         {
+            TcpClient client = new TcpClient(serverIP, port);
+            int byteCount = Encoding.ASCII.GetByteCount("banaan/33/22/B/fruit");
 
+            byte[] sendData = new byte[byteCount];
+            sendData = Encoding.ASCII.GetBytes("banaan/33/22/B/fruit");
+
+            NetworkStream stream = client.GetStream();
+            stream.Write(sendData, 0, sendData.Length);
+
+            stream.Close();
+            client.Close();
+        }
+
+
+        public string ReceiveData()
+        {
+            TcpClient client = new TcpClient(serverIP, port);
+            NetworkStream stream = client.GetStream();
+            byte[] serverMessage = new byte[100];
+            string serverMessageString;
+
+            do
+            {
+                stream.Read(serverMessage, 0, serverMessage.Length);
+                serverMessageString = Encoding.ASCII.GetString(serverMessage);               
+            }
+            while (stream.DataAvailable);
+
+            // Stop the connection
+            stream.Close();
+            client.Close();
+
+            return serverMessageString;
         }
     }
 }
